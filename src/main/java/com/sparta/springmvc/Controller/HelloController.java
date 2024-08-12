a4eb736061712a808374650c2f84a3b9456a4e99
taehyunkim <98rlaxogus@gmail.com>
2024-08-12 20:49:50 +0900

    controller mapping practice

--- src/main/java/com/sparta/springmvc/Controller/HelloController.java
+++ src/main/java/com/sparta/springmvc/Controller/HelloController.java
@@ -1,17 +1,40 @@
 package com.sparta.springmvc.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/api") //맵핑 경로를 지정한 이름으로 통일시켜준다
 public class HelloController {
 
-    @GetMapping("api/hello")
+    @GetMapping("/hello")
     @ResponseBody
     public String hello(){
-        return"Hello World";
+        return"Hello World!";
+    }
+
+    @GetMapping("/get")
+    @ResponseBody
+    public String get(){
+        return"Get Method Request";
+    }
+
+    @PostMapping("/post")
+    @ResponseBody
+    public String post(){
+        return ("Post Method Request");
+    }
+
+    @PutMapping("/put")
+    @ResponseBody
+    public String put(){
+        return("Put Method Request");
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public String delete(){
+        return("Delete Method Request");
     }
 
 }
